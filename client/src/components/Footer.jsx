9e26415f4db985878c987353eef92ff20e632ce7
Kevin Anadon <kevinxd77200@gmail.com>
2023-08-11 13:54:03 -0300

    feat(client): add footer hover effect and for small devices only displays img of github

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,9 +1,43 @@
 import githubSVG from "../assets/resources/images/github.svg"
+import "./Footer.css"
+import { useState, useEffect } from "react"
+import "bootstrap/dist/css/bootstrap.min.css"
 
 export const Footer = () => { 
+  const [isMobileView, setIsMobileView] = useState(false)
+  const [isHovered, setIsHovered] = useState(false)
+
+  const handleResize = () => {
+    setIsMobileView(window.innerWidth <= 600)
+  }
+
+  const handleMouseEnter  = () => {
+    setIsHovered(true)
+  }
+
+  const handleMouseLeave  = () => {
+    setIsHovered(false)
+  }
+
+  useEffect(() => {
+    handleResize()
+    window.addEventListener("resize", handleResize)
+    return () => {
+      window.removeEventListener("resize", handleResize)
+    }
+  })
+
   return (
     <footer>
-      Open source coded by <a href="https://github.com/kevin-anadon"><img src={githubSVG} alt="Github Icon"></img> Kevin Anadon </a>
+      <a 
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        className="d-flex justify-content-center align-items-center" href="https://github.com/kevin-anadon">
+          <img src={githubSVG} alt="Github Icon"></img>
+          {(isHovered && !isMobileView) ? (
+            <p className="mb-0">Open Source Coded by Kevin Anadon</p>
+          ) : null}
+      </a>
     </footer>
   )
 }
